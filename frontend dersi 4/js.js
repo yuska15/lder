039a375f4e3597d82339328ee16d850242db15cf
yuska15 <yusifmamedov0212@gmail.com>
2025-05-01 19:08:20 +0400

    Add files via upload

---
+++ frontend dersi 4/js.js
@@ -0,0 +1,7 @@
+var email = prompt("Email daxil edin:");
+var domain = (email || "").split("@")[1] || "";
+alert("Domen:"+domain);
+const cümle = "Salam dünya";
+const soz = "Dünya";
+const nəticə = cumle.includes(söz);
+console.log(nəticə);
